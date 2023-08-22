61c14bc8a742dc14a4a561b0082e827e2cd5aced
Shiam Chowdhury <shiam.sust@gmail.com>
2023-08-22 13:13:19 +0600

    initial

---
+++ fluent-shortcode.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Plugin Name:       Fluent Shortcode
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.3
+ * Author:            Shiam Chowdhury
+ * Author URI:        https://shiamch.com/
+ */
+
+define('FLUENT_SHORTCODE', 'fluent-shortcode');
+
+register_activation_hook(__FILE__, );
+
+register_deactivation_hook(__FILE__, );
